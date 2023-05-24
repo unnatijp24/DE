f92c536fec4838b52e4817b52ef13968d6a6c982
Unnati <unnatijp24@gmail.com>
2023-05-24 20:05:23 +0530

    git commit-m "hello"

--- login-signup.php
+++ login-signup.php
@@ -1,3 +1,43 @@
+<?php
+
+if(isset($_POST['fmail'])){
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "mydb";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+
+    if(!$con){
+        die("connection to this database failed due to " . mysqli_connect_error());
+    }
+   //  else{
+   //    echo "connected";
+   //  }
+
+    $fmail = $_POST['fmail'];
+    $fpassword = $_POST['fpassword'];
+    $cpassword = $_POST['cpassword'];
+   // echo $fmail;
+   // echo $fpassword;
+   if($cpassword == $fpassword){
+      $sql = "INSERT INTO `log` (`fmail`, `fpassword`, `dt`) VALUES ('$fmail', '$fpassword', current_timestamp());";
+
+    if(mysqli_query($con, $sql)){
+        header("Location:Homepage.html");
+    }
+    else{
+        echo "ERROR: $sql1 <br> $con->error";
+    }
+   }
+    
+
+   //  $con-> close();
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -6,8 +46,9 @@
       <title>Login and Registration Form</title>
       <link rel="stylesheet" href="login-signupCSS.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
    </head>
-   <body>
+   <body style="background: -webkit-linear-gradient(left, #1996e7, #ebe9ec);">
       <div class="wrapper">
          <div class="title-text">
             <div class="title login">
@@ -26,16 +67,16 @@
                <div class="slider-tab"></div>
             </div>
             <div class="form-inner">
-               <form action="Homepage.html" class="login">
+               <form action="login.php" class="login" method="POST">
                   <div class="field">
-                     <input type="text" placeholder="Name" required>
+                     <input type="mail" placeholder="E-mail" name="fmail" required>
                   </div>
                   <div class="field">
-                     <input type="password" placeholder="Password" required>
+                     <input type="password" placeholder="Password" name="fpassword" required>
                   </div>
-                  <div class="pass-link">
-                     <a href="#">Forgot password?</a>
-                  </div>
+                  <!-- <div class="pass-link">
+                     <a href="forgotpwd.php">Forgot password?</a>
+                  </div> -->
                   <div class="field btn">
                      <div class="btn-layer"></div>
                      <input type="submit" value="Login">
@@ -44,15 +85,15 @@
                      Don't have an Account? <a href="">Signup</a>
                   </div>
                </form>
-               <form action="#" class="signup">
+               <form action="login-signup.php" class="signup" method="POST">
                   <div class="field">
-                     <input type="text" placeholder="Name" required>
+                     <input type="mail" placeholder="E-mail" name="fmail" required>
                   </div>
                   <div class="field">
-                     <input type="password" placeholder="Password" required>
+                     <input type="password" placeholder="Password" name="fpassword" required>
                   </div>
                   <div class="field">
-                     <input type="password" placeholder="Confirm password" required>
+                     <input type="password" placeholder="Confirm password" name="cpassword" required>
                   </div>
                   <div class="field btn">
                      <div class="btn-layer"></div>
@@ -62,6 +103,7 @@
             </div>
          </div>
       </div>
+      
       <script>
          const loginText = document.querySelector(".title-text .login");
          const loginForm = document.querySelector("form.login");
